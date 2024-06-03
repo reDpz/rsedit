5f4fef2d219bf80ff4641cff50cd539f5e6ae779
redpz <sv_redpz@outlook.com>
2024-06-03 21:57:17 +0100

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
+use gtk::{
+    glib, Application, ApplicationWindow, Box as GtkBox, Button, DropDown, HeaderBar, Label,
+    Orientation, Statusbar, TextBuffer, TextView,
+};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld2";
 
@@ -15,26 +18,69 @@ fn main() -> glib::ExitCode {
 }
 
 fn build_ui(app: &Application) {
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
+    // vbox holding everything
+    let vbox = GtkBox::new(Orientation::Vertical, 0);
+
+    // button for opening files
+    let open_button = Button::builder()
+        .label("Open file")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    // add the functionlity
+    open_button.connect_clicked(|_| println!("Opening file"));
+
+    // add it into a dropdown
+    let dropdown_opener = DropDown::builder()
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(|button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
-    });
+    open_button.set_parent(&dropdown_opener);
+
+    // Make header bar
+    let title_label = Label::builder().label("rsedit").build();
+    let headerbar = HeaderBar::builder()
+        .decoration_layout("menu:close")
+        .title_widget(&title_label)
+        .build();
+
+    let statusbar_buffer_text: &str = "[Untitled buffer]";
+    // Create our status bar to display name of buffer
+    let statusbar = Statusbar::builder().build();
+    statusbar.push(0, &statusbar_buffer_text);
+    statusbar.set_height_request(50);
+
+    // Create a button with label and margins
+    // example button
+
+    // create buffer to store our text in
+    let text_buffer = TextBuffer::builder().build();
+
+    // Create the text view
+    let text_view = TextView::builder()
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .buffer(&text_buffer)
+        .build();
+
+    // add our vbox elements
+    vbox.append(&headerbar);
+    vbox.append(&dropdown_opener);
+    vbox.append(&text_view);
+    vbox.append(&statusbar);
 
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
-        .child(&button)
+        .child(&vbox)
         .build();
 
     // Present window
